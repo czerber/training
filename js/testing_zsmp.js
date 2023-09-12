b085e22e3d3d6b2cb58402f3ab31b73bfec1bfc2
czerber <103359770+czerber@users.noreply.github.com>
2023-09-12 10:28:19 +0300

    Add files via upload

---
+++ js/testing_zsmp.js
@@ -0,0 +1,6 @@
+const result = confirm('Данный блок находится в разработке');
+if (result) {
+    window.location.href = '../home_page/index.html';
+} else {
+    window.location.href = '../home_page/index.html';
+}
